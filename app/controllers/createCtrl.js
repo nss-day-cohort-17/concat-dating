bfa70b7b8fb1a20b98e4e68bb7347bb40ac219cc
Michael Tambornino <michael.tambornino@gmail.com>
2017-04-12 14:38:54 -0500

    destructuring req

--- app/controllers/createCtrl.js
+++ app/controllers/createCtrl.js
@@ -6,11 +6,12 @@ module.exports.show = (req, res) => {
   res.render("createUser", {page: "Create"})
 }
 
-module.exports.createUser = (req, res) => {
-  User.forge()
+module.exports.createUser = ({body: {username, password, picUrl, likes, dislikes}}, res) => {
+  return User.forge({username, password, picUrl, likes, dislikes})
     .save()
     .then( (newUser) => {
       console.log("newUser:", newUser)
     })
+    .catch((err) => console.log(`Couldn't save new user`, err))
     .then( () => res.redirect('/home'))
 }
