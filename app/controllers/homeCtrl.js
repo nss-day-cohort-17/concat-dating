750add52f18f0aa188c53905053eaf8417b3f71c
Michael Tambornino <michael.tambornino@gmail.com>
2017-04-12 14:39:26 -0500

    commenting out for later

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -3,10 +3,10 @@
 const User = require('../models/userModel')
 
 module.exports.show = (req, res) => {
-    User.forge()
-    .fetch('*')
-    .then( (data) => {
-      console.log("allUsers:", data)
-    })
+    // User.forge()
+    // .fetch('*')
+    // .then( (data) => {
+    //   console.log("allUsers:", data)
+    // })
     res.render("home", {page: "Home"})
 }
--- app/models/userModel.js
+++ app/models/userModel.js
@@ -4,12 +4,12 @@ const { bookshelf } = require('../db/database.js')
 
 const User = bookshelf.Model.extend(
   {
-    tableName: 'users',  // defines the model relationship
-    hasTimestamps: true, // automatically
-  },
-  {
-    // optional methods
+    tableName: 'users'  // defines the model relationship
+    // hasTimestamps: true
   }
+  // {
+  //   // optional methods
+  // }
 )
 
 module.exports = User
