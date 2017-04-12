b4ae2504518b08aedd6d640952d77fa42c8c2027
Michael Tambornino <michael.tambornino@gmail.com>
2017-04-12 13:16:21 -0500

    function to fetch all users

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,5 +1,12 @@
 'use strict'
 
+const User = require('../models/userModel')
+
 module.exports.show = (req, res) => {
+    User.forge()
+    .fetch('*')
+    .then( (data) => {
+      console.log("allUsers:", data)
+    })
     res.render("home", {page: "Home"})
 }
