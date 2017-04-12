6228cb401b9a08a95a947c3e6034c1a3aa3392f1
Michael Tambornino <michael.tambornino@gmail.com>
2017-04-12 13:16:58 -0500

    setting up instance of database

--- app/db/database.js
+++ app/db/database.js
@@ -1,11 +1,13 @@
 'use strict'
 
-console.log(`database file running`)
+const env = process.env.NODE_ENV || 'development';
 
-const config = require('../../knexfile')['development']
+const config = require('../../knexfile')[env]
 const knex = require('knex')(config)  // passing the config variable to knex function
 const bookshelf = require('bookshelf')(knex)  // passing the knex variable to bookshelf function
 
-console.log("bookshelf:", bookshelf)
+console.log("bookshelf require successful")
+
+bookshelf.plugin('registry');  // helps to avoid circular dependency
 
 module.exports = { knex, bookshelf }
