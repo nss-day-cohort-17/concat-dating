867fd1e78bb8d61d00ebf9f5ae4bc8fd8cc5f062
TSturtledove <roberttortorelli@yahoo.com>
2017-04-13 10:33:18 -0500

    filled out passport-strategies, and userModel. Part way done with createUser'

--- app/controllers/createCtrl.js
+++ app/controllers/createCtrl.js
@@ -3,15 +3,25 @@
 const User = require('../models/userModel')
 
 module.exports.show = (req, res) => {
-  res.render("createUser", {page: "Create"})
+  res.render('createUser', {page: 'Create'})
 }
 
 module.exports.createUser = ({body: {username, password, picUrl, likes, dislikes}}, res) => {
-  return User.forge({username, password, picUrl, likes, dislikes})
-    .save()
-    .then( (newUser) => {
-      console.log("newUser:", newUser)
+  if (password === confirmPassword) {
+    User.findOneByUsername(username)//this takes the input username and compares it to the usernames in the database
+    // the returned data "user" will be the same username if it exists in the database or a null value if it has yet to be made
+    .then( (user) => { //if the username already exists the following "if" statement will fire and stop a duplicate from being made
+      if (user) return res.render('createUser', {msg: 'Username already used, please try another one'})
+      //if the username has not been used yet
+      return User.forge({username, password, picUrl, likes, dislikes})
+      .save()
+      .then( ()=> {
+        res.redirect('/home')
+      })
+      .catch( (err)=> res.render('createUser', {msg: 'Sorry, there was a problem with saving theis user. Please try again'}))
     })
-    .catch((err) => console.log(`Couldn't save new user`, err))
-    .then( () => res.redirect('/home'))
+    .catch( (err)=> res.render('createUser', {msg: 'Sorry, we could tell if this username had been taken or not.  Please try again'}))
+  } else {
+    res.render('createUser', {msg: "password and password confirmation don't match, try again"})
+  }
 }
--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,15 +1,30 @@
 'use strict'
 
-const { bookshelf } = require('../db/database.js')
+const { bookshelf } = require('../db/database')
+const { compare } = require('bcryptjs')
 
-const User = bookshelf.Model.extend(
-  {
-    tableName: 'users'  // defines the model relationship
-    // hasTimestamps: true
-  }
-  // {
-  //   // optional methods
-  // }
-)
+const User = bookshelf.Model.extend({
+    tableName: 'users',  // defines the model relationship
+    bcrypt: {field; 'password'}, //tells the program to hash the input password
+    comparePassword: function (passwordString) {
+      //for the following return line "this.attributes" is the user information that has been
+      // pulled from the database, so the "this.attributes.password" is the password
+      // that was saved on the database for the user in question.  So with this line we
+      // are comparing the input password "passwordString" with the one from the database
+      // to make sure that it is the right user trying to login
+      return compare(passwordString, this.attributes.password)
+    }
+  }, {
+    findOneByUsername: function (username) {
+      return this.forge({username})//
+      .fetch()
+      .then( (user) => {
+        return user;
+      })
+      .catch( ()=> {
+        return(null)
+      });
+    }
+  });
 
 module.exports = User
